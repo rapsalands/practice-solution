32f1667e3473883d58409855fb0367463c3ddc55
Sandeep Singh <sandeepsingh13@outlook.com>
2022-01-29 23:41:52 -0800

    Initial Commit.

---
+++ src/solutions/multiImageCarousal.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MultiImageCarousal: React.FC<Props> = ({ ...props }) => {
+
+    return (
+        <React.Fragment>
+            Multi Image Corousal
+        </React.Fragment>
+    );
+}
+
+export default MultiImageCarousal;
+
+interface Props {
+    
+}
---
+++ src/solutions/singleImageCarousal.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SingleImageCarousal: React.FC<Props> = ({ ...props }) => {
+
+    return (
+        <React.Fragment>
+            Single Image Carousal
+        </React.Fragment>
+    );
+}
+
+export default SingleImageCarousal;
+
+interface Props {
+    
+}
