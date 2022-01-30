b1452374095828ad63199807276f2a3d3bd6c4fa
Sandeep Singh <sandeepsingh13@outlook.com>
2022-01-30 01:15:44 -0800

    SingleImage Carousal working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
 import MultiImageCarousal from './solutions/multiImageCarousal';
+import SingleImageCarousal from './solutions/singleImageCarousal';
 
 function App() {
   return (
     <div className="App">
-      <MultiImageCarousal timeLag={2000} sources={["A", "B", "C", "D"]} />
+      {/* <MultiImageCarousal timeLag={2000} sources={["A", "B", "C", "D"]} /> */}
+      <hr />
+      <SingleImageCarousal sources={["A", "B", "C", "D"]} />
     </div>
   );
 }
--- src/solutions/singleImageCarousal.tsx
+++ src/solutions/singleImageCarousal.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
 
-const SingleImageCarousal: React.FC<Props> = ({ ...props }) => {
+const SingleImageCarousal: React.FC<Props> = ({ sources, lagTime = 1000, ...props }) => {
+
+    const [counter, setCounter] = React.useState<number>(0);
+    const [pause, setPause] = React.useState<boolean>(false);
+    const [intIns, setIntIns] = React.useState<any>();
+
+    React.useEffect(() => {
+
+        if(pause) {
+            clearInterval(intIns);
+            return;
+        }
+
+        const interval = setInterval(() => {
+            setCounter(prevCounter => {
+                const sum = prevCounter + 1;
+                if (sum == sources.length) return 0;
+                return sum;
+            });
+
+            setIntIns(interval);
+
+            return () => {
+                if(intIns) clearInterval(intIns);
+            };
+
+        }, lagTime);
+    }, [pause]);
+
+    const pauseClick = (e: any) => setPause(!pause);
 
     return (
         <React.Fragment>
             Single Image Carousal
+            <br />
+            <button onClick={pauseClick}>{ pause ? 'Start' : 'Pause' }</button>
+            <br />
+            {
+                sources[counter]
+            }
         </React.Fragment>
     );
 }
@@ -12,5 +47,6 @@ const SingleImageCarousal: React.FC<Props> = ({ ...props }) => {
 export default SingleImageCarousal;
 
 interface Props {
-    
+    sources: string[],
+    lagTime?: number
 }
