ae785c49b990c1b86e5664cf4bf87bcb13d6d0fe
Sandeep Singh <sandeepsingh13@outlook.com>
2022-01-30 01:16:54 -0800

    Template components.

---
+++ src/solutions/fileTree.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const FileTree: React.FC<Props> = ({ ...props }) => {
+
+    return (
+        <React.Fragment>
+            File Tree
+        </React.Fragment>
+    );
+}
+
+export default FileTree;
+
+interface Props {
+    
+}
---
+++ src/solutions/ratingApp.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const RatingApp: React.FC<Props> = ({ ...props }) => {
+
+    return (
+        <React.Fragment>
+            Rating App
+        </React.Fragment>
+    );
+}
+
+export default RatingApp;
+
+interface Props {
+    
+}
