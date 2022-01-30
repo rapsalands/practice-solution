5e84d4868797c7021dbdaa3e0e88556e1ebd4565
Sandeep Singh <sandeepsingh13@outlook.com>
2022-01-30 00:53:43 -0800

    Pause functionality working.

--- src/solutions/multiImageCarousal.tsx
+++ src/solutions/multiImageCarousal.tsx
@@ -3,22 +3,35 @@ import React from 'react';
 const MultiImageCarousal: React.FC<Props> = ({ timeLag = 1000, sources, imageCount = 3, ...props }) => {
 
     const [counter, setCounter] = React.useState<number>(0);
+    const [pause, setPause] = React.useState<boolean>(false);
+    const [intIns, setIntIns] = React.useState<any>();
 
     React.useEffect(() => {
+
+        if(pause) {
+            clearInterval(intIns);
+            return;
+        }
 
         const interval = setInterval(() => {
             changeImage(null, 1);
         }, timeLag);
 
+        if(!intIns) {
+            setIntIns(interval);
+        }
+
         return () => {
             clearInterval(interval);
         }
-    }, []);
+    }, [pause]);
 
     const range: number[] = Array(imageCount).fill(null).map((_, i) => i);
 
     function changeImage(e: any, dir: number) {
         setCounter(prevCounter => {
+            console.log(`Pause: ${pause}`);
+            
             const sum = prevCounter + dir;
             if (sum < 0) return imageCount;
             if (sum > imageCount) return 0;
@@ -30,9 +43,13 @@ const MultiImageCarousal: React.FC<Props> = ({ timeLag = 1000, sources, imageCou
         return sources[(index + sources.length) % sources.length];
     }
 
+    const pauseClick = (e: any) => setPause(!pause);
+
     return (
         <React.Fragment>
             Multi Image Corousal
+            <br />
+            <button onClick={pauseClick}>{pause ? 'Start' : 'Pause'}</button>
             <br />
             <button onClick={(e) => changeImage(e, -1)}>Previous</button>
             {
