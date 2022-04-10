773814e758cab84b66e1a334ee827e6b39a5c9d6
Sandeep Singh <sandeepsingh13@outlook.com>
2022-04-10 03:23:48 -0700

    Nested Checkbox.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css';
 import ArrayAsTable from './reactSolutions/arrayAsTable';
 import FileTree from './reactSolutions/fileTree';
 import MultiImageCarousal from './reactSolutions/multiImageCarousal';
+import NestedCheckbox from './reactSolutions/nestedCheckbox';
+import NestedCheckbox1 from './reactSolutions/nestedCheckbox_JavaScript';
 import RatingApp from './reactSolutions/ratingApp';
 import SingleImageCarousal from './reactSolutions/singleImageCarousal';
 import Stack from './reactSolutions/stack';
@@ -9,6 +11,10 @@ import Stack from './reactSolutions/stack';
 function App() {
   return (
     <div className="App">
+      <NestedCheckbox />
+      <hr />
+      <NestedCheckbox1 />
+      <hr />
       <ArrayAsTable source={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} />
       <hr />
       <Stack />
