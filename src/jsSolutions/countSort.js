dff8929597b9e21ea517418c7317ebe875e11051
Sandeep Singh <sandeepsingh13@outlook.com>
2022-04-11 01:16:18 -0700

    Count Sort.

---
+++ src/jsSolutions/countSort.js
@@ -0,0 +1,45 @@
+Array.prototype.countSort = function () {
+    function findMinMax(arr) {
+        let min = arr[0], max = arr[0];
+        arr.forEach(el => {
+            min = Math.min(min, el);
+            max = Math.max(max, el);
+        });
+        return { min, max };
+    }
+
+    function findFrequency(arr) {
+        const freq = {};
+        arr.forEach(el => {
+            if (freq[el] === undefined) freq[el] = 0;
+            freq[el]++;
+        });
+        return freq;
+    }
+
+    function constructArr(min, max, freq) {
+        const keys = new Set(Object.keys(freq).map(n => +n));
+        const result = [];
+
+        while(min <= max) {
+            if(keys.has(min)) {
+                let count = 0;
+                while(count++ < freq[min]) {
+                    result.push(min);
+                }
+            }
+            min++;
+        }
+
+        return result;
+    }
+
+    const { min, max } = findMinMax(this);
+    const freq = findFrequency(this);
+    return constructArr(min, max, freq);
+}
+
+const arr = [4, 87, 23, 64, 846, 5, 8, 7, 12, 5, 64, 47, 2, 3, 6, 64, 5];
+
+console.log([...arr].sort((a, b) => a - b));
+console.log([...arr].countSort());
